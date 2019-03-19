1d6c02a23f2e58b6c5441a546f0f2d396dd1bcec
simsingh293 <30273904+simsingh293@users.noreply.github.com>
2019-03-19 14:15:16 -0400

    Commit project to Github

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour {
+    // SerializeField makes the component available in the inspector
+    [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+
+    State state;
+
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.GetStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        ManageState();
+	}
+
+    private void ManageState()
+    {
+        // can use keyword var if the type of the object you're trying to return is already defined
+        var nextStates = state.GetNextStates();
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = nextStates[1];
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            state = nextStates[2];
+        }
+        textComponent.text = state.GetStateStory();
+    }
+}
+
+
